ff9f284953bdd43ed423797f89fdcb5042affb6a
alan <Drake@DESKTOP-2C494C7>
2025-10-27 23:18:33 -0400

    ultima

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -8,16 +8,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     */
     public function handle(Request $request, Closure $next, $role): Response
     {
         if (!auth()->check()) {
             return redirect()->route('login');
         }
 
-        if (auth()->user()->rol !== $role) {
+        if (auth()->user()->rol != $role) {
             abort(403, 'No tienes permisos para acceder a esta página.');
         }
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,17 +1,9 @@
 <?php
 
-use App\Http\Middleware\CheckRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app->middleware([
-    \Illuminate\Http\Middleware\TrustProxies::class,
-    \Illuminate\Http\Middleware\HandleCors::class,
-]);
-
-$app->withFacades();
-$app->withEloquent();
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -19,9 +11,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Middleware global
+        $middleware->web(append: [
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
+
         // Registrar middleware personalizado
         $middleware->alias([
-            'role' => CheckRole::class,
+            'role' => \App\Http\Middleware\CheckRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
